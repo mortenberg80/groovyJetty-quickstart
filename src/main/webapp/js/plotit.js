d41de0c6a5e4304e02d34ae560f7bec7b296d1a3
Morten Berg <mortenberg80@gmail.com>
2013-06-18 06:08:07 +0200

    Changed to barcharts

--- src/main/webapp/js/plotit.js
+++ src/main/webapp/js/plotit.js
@@ -1,8 +1,8 @@
 $(function() {
     $.getJSON("stats.groovy", function(data) {
-        var behandlet = {data: data["behandlet"], label: "Behandlet"};
-        var epost = {data: data["epost"], label: "Epost", yaxis: 2};
-        var sms = {data: data["sms"], label: "Sms"};
+        var behandlet = {data: data["behandlet"], label: "Behandlet", bars: { show: true, align: "left", barWidth: 8*60*60*1000}};
+        var epost = {data: data["epost"], label: "Epost",  bars: { show: true, align: "center", barWidth: 8*60*60*1000}};
+        var sms = {data: data["sms"], label: "Sms", bars: { show: true, align: "right", barWidth: 8*60*60*1000}};
 
         $.plot("#placeholder", [ behandlet, epost, sms ], {
             xaxis: { mode: "time" },
